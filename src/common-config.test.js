cf7ce97aeed56232b46e8ecfea323937e35e39d8
Michael "Z" Goddard <mzgoddard@gmail.com>
2017-06-16 03:49:52 -0400

    Add config-testMatch fixture

--- src/common-config.test.js
+++ src/common-config.test.js
@@ -1,7 +1,14 @@
-const {run, itBuilt, itSkips} = require('../fixtures/utils');
+const {run, itBuilt, itSkips, itPasses, itFails} = require('../fixtures/utils');
 
 it('bail', () => {
   return run('config-bail')
   .then(itBuilt(['src/entry.test.js']))
-  .then(itSkips(['passes']));
+  .then(itSkips(['passes']))
+  .then(itFails);
+}, 30000);
+
+it('testMatch', () => {
+  return run('config-testMatch')
+  .then(itBuilt(['src/entry.test.js', 'src/entry.test2.js']))
+  .then(itPasses);
 }, 30000);
