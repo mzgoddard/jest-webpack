cef6eb576fd75ecc176251cece5feae5e160d068
Michael "Z" Goddard <mzgoddard@gmail.com>
2017-12-23 18:48:19 -0500

    feat(flags): Break up flags into jest and webpack groups
    Break up command line flags at `--` or `--webpack` and pass the
    following options to webpack using `webpack/bin/config-yargs`. Prior
    options continue to jest's options.
    
    Use `webpack/bin/convert-argv` to further handle the options and return
    a webpack configuration.

--- jest-webpack.js
+++ jest-webpack.js
@@ -1,24 +1,49 @@
 #!/usr/bin/env node
 
-var join = require('path').join;
-
-function config(dir) {
-  if (!dir) {
-    dir = process.cwd();
-  }
-  var config;
-  try {
-    var resolved = require.resolve(join(dir, 'webpack.config.babel.js'));
-    require('babel-register');
-    config = require(resolved);
-  }
-  catch (_) {
-    config = require(join(dir, 'webpack.config.js'));
-  }
-  return config;
+function versionInfo() {
+  return `jest-webpack ${require('./package.json').version}\n` +
+  `jest ${require('jest/package.json').version}\n` +
+  `webpack ${require('webpack/package.json').version}`;
 }
 
 function run(argv, webpackConfig) {
+  var jestArgvPortion, webpackArgvPortion;
+  var webpackArgIndex =
+    ~(~argv.lastIndexOf('--') || ~argv.lastIndexOf('--webpack'));
+  if (webpackArgIndex === -1) {
+    jestArgvPortion = argv;
+    webpackArgvPortion = [];
+  }
+  else {
+    jestArgvPortion = argv.slice(0, webpackArgIndex);
+    webpackArgvPortion = argv.slice(webpackArgIndex);
+  }
+
+  if (!webpackConfig) {
+    var webpackYargs = require('yargs')([]);
+    require('webpack/bin/config-yargs')(webpackYargs);
+    var webpackArgv = webpackYargs.parse(webpackArgvPortion);
+    webpackConfig = require('webpack/bin/convert-argv')(
+      webpackYargs, webpackArgv
+    );
+  }
+
+  if (typeof webpackConfig.then === 'function') {
+    webpackConfig.then(function(config) {run(argv, config);}, function(err) {
+      console.error(err.stack || err);
+      process.exit();
+    });
+    return;
+  }
+
+  if (Array.isArray(webpackConfig)) {
+    console.error(
+      'jest-webpack does not support webpack\'s multi-compiler array ' +
+      'configuration at this time.'
+    );
+    process.exit(1);
+  }
+
   var main;
   if (require('webpack/package.json').version.startsWith('1')) {
     main = require('./webpack-1/jest-webpack.js');
@@ -31,16 +56,15 @@ function run(argv, webpackConfig) {
     process.env.NODE_ENV = 'test';
   }
 
-  if (!webpackConfig) {
-    webpackConfig = config();
-  }
-
   main(argv, webpackConfig);
 }
 
 if (process.mainModule === module) {
+  const jestWebpackYargs = require('yargs')(process.argv)
+    .usage(`${versionInfo()}\nUsage: jest-webpack [jest options] [--[webpack] [webpack options]]`);
+  jestWebpackYargs.parse();
+
   run(process.argv.slice(2));
 }
 
 module.exports = run;
-run.webpackConfig = config;
